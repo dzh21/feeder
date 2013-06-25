828530179aee38924389a0ac2495a7f765284f46
dzh <dzh@dzhbox>
2013-06-25 15:43:54 +0300

    - отказался от django_cron в пользу стандартного cron - почистил вывод сообщений о парсинге фидов

--- reader/management/commands/checknews.py
+++ reader/management/commands/checknews.py
@@ -11,19 +11,21 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		feeds = Feed.objects.all()		
 		for feed in feeds:						
+			print 'start parsing %s' % feed.link
 			d = feedparser.parse(feed.link)						
 			skip_cnt = 0
 			for item in d.entries:				
 				news_line = NewsLine()
 				news_line.feed_id = feed.id
-				news_line.link_id = item.id
+				news_line.link_id = item.link
 				news_line.title = item.title
-				news_line.published = timezone.make_aware(datetime.datetime(*item.published_parsed[:-3]), timezone.get_current_timezone())
+				#news_line.published = timezone.make_aware(datetime.datetime(*item.published_parsed[:-3]), timezone.get_current_timezone())
+				news_line.published = timezone.make_aware(datetime.datetime(*item.updated_parsed[:-3]), timezone.get_current_timezone())
 				news_line.summary = item.summary
 				try:
 					news_line.save()
 				except Exception, e:
 					#print 'error: %r' % e		
 					skip_cnt += 1
-			print 'feed %s parsed: add %d, skip %d' % (feed.link, len(d.entries) - skip_cnt, skip_cnt)
+			print 'parsed: add %d, skip %d' % (len(d.entries) - skip_cnt, skip_cnt)
 				
