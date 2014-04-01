431f4e315e81b6bfdb0008329f3871350ce1fb4a
dzh <devnull@localhost>
2014-04-01 10:46:20 +0300

    - правка очистки - чистим только прочитанные

--- reader/management/commands/clearbase.py
+++ reader/management/commands/clearbase.py
@@ -9,6 +9,6 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		old_cnt = NewsLine.objects.all().count()
-		NewsLine.objects.exclude(published__gt=datetime.datetime.now() - datetime.timedelta(weeks=4)).delete()
+		NewsLine.objects.exclude(published__gt=datetime.datetime.now() - datetime.timedelta(weeks=4), readed=True).delete()
 		print "Lines deleted count: %d" % (old_cnt - NewsLine.objects.all().count())
 				
